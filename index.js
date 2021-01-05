ae63e754b56019af9c71cc716367b28b0392eb60
jfernandezlafite <61310767+jfernandezlafite@users.noreply.github.com>
2021-01-05 09:36:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ mongoose.connection.on("error", function (error) { console.log(error)})
 
 app.use("/imagenes/", imagenesRoutes);
 app.use("/imagenes/:id", imagenesRoutes);
-app.use("/imagenes/upload", imagenesRoutes);
 const port = process.env.PORT || config.Port;
 
 app.listen(port,function(req,res){
