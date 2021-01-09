7e8e9da2692192303e94ac392eef2d95b0856203
jfernandezlafite <61310767+jfernandezlafite@users.noreply.github.com>
2021-01-09 20:54:56 +0100

    Add files via upload

--- config/multer.js
+++ config/multer.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req,file,cb) =>{
-        //cb(null,"https://application-0-emvrx.mongodbstitch.com/assets/images/")
-        cb(null,path.join(__dirname,'../app/upload'))
+        cb(null,"https://stitch-statichosting-prod.s3.amazonaws.com/5f912719421334c99e7d637b/assets/images/")
+        //cb(null,path.join(__dirname,'../app/upload'))
     },
     filename: (req,file,cb)=>{
         cb(null,`${file.originalname}`)
